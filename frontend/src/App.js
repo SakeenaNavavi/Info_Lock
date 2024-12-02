8cc9abc38beec9a7adc5126d14d98775a24a6b02
SakeenaNavavi <Sakeenanavavi@gmail.com>
2024-12-03 03:37:42 +0530

    build issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,6 @@ import LoginPage from './Pages/LoginPage';
 import VerifyEmail from './Components/VerifyEmail';
 import AdminLoginPage from './Pages/AdminLoginPage';
 import AdminDashboard from './Pages/AdminDashboard';
-import Dashboard from './Pages/Dashboard';
 
 function App() {
   return (
@@ -19,7 +18,6 @@ function App() {
         <Route path="/verify-email/:token" element={<VerifyEmail />} />
         <Route path="/admin-login" element={<AdminLoginPage />} />
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
-        <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
     </div>
