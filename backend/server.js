f7f6fc332bfba704e1e4150a7acc38eb4f3307f5
SakeenaNavavi <Sakeenanavavi@gmail.com>
2024-12-06 09:11:43 +0530

    server fix

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,20 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://sakeenanavavi.me');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    
+    next();
+});
+
 const corsOptions = {
     origin: function (origin, callback) {
         const allowedOrigins = [
