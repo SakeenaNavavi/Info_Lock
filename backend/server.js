eefa2ab6f847859a8d9456f32b51f595b6761580
SakeenaNavavi <Sakeenanavavi@gmail.com>
2024-12-06 10:18:47 +0530

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://sakeenanavavi.me,https://info-lock-sakeenanavavis-projects.vercel.app/');  // Allow all origins (temporary)
+    res.header('Access-Control-Allow-Origin', 'https://sakeenanavavi.me');  // Allow all origins (temporary)
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
